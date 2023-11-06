8294db1144e16437d3bdea6ed3549c0c3f9df811
CMamba <96987448+cyber-mamba@users.noreply.github.com>
2023-11-07 00:09:30 +0900

    Update windows-memory-management.c

--- MalDev/windows-memory-management.c
+++ MalDev/windows-memory-management.c
@@ -1,3 +1,5 @@
+// allocate new address
+
 # include <Windows.h>
 # include <stdio.h>
 
@@ -12,4 +14,44 @@ int main() {
 
 	return 0;
 
+}
+
+// copy strings and allocate new address
+
+#include <Windows.h>
+#include <stdio.h>
+
+int main() {
+	PVOID pAddress = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, 100);
+	CHAR* cString = "MalDev Academy Is The Best";
+
+	memcpy(pAddress, cString, strlen(cString)); // mov pAddress, cString
+	printf("[+] Address Of cString : 0x%p \n", cString);
+
+	printf("[+] Base Address Of Allocated Memory : 0x%p \n", pAddress);
+	printf("[+] Address Of cString : 0x%p \n", cString);
+
+	printf("[#] Press <Enter> To Quit ... ");
+	getchar();
+
+	return 0;
+}
+
+
+// heapfree
+
+#include <Windows.h>
+#include <stdio.h>
+
+int main() {
+	PVOID pAddress = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, 100);
+
+	CHAR* cString = "MalDev Academy Is The Best";
+	memcpy(pAddress, cString, strlen(cString)); // mov pAddress, cString
+
+	printf("[+] Base Address Of Allocated Memory : 0x%p \n", pAddress); // still allocated memory address.
+
+	HeapFree(GetProcessHeap(), 0, pAddress); // address is free!
+
+	return 0;
 }
