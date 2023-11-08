ee2948a336311e252e0123d60cad59731b133720
CMamba <96987448+cyber-mamba@users.noreply.github.com>
2023-11-09 05:31:25 +0900

    Update 9_Dynamic-Link Library(DLL).c

--- MalDev/9_Dynamic-Link Library(DLL).c
+++ MalDev/9_Dynamic-Link Library(DLL).c
@@ -6,10 +6,25 @@ ntdll.dll, kernel32.dll, kernelbase.dll, these are also DLL and export functions
 We need to load DLL into its address space.
 */
 
-/*
-Why use DLLs?
+
+/* source code for creating dll */
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include <Windows.h>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
 
 
-
-
-*/
